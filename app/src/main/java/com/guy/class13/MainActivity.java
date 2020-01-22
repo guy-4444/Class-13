c60641c59dfe72b51e6adc45628f3cb7ccb67eee
Guy <guyskv@gmail.com>
2020-01-22 19:17:09 +0200

    Initial commit

---
+++ app/src/main/java/com/guy/class13/MainActivity.java
@@ -0,0 +1,28 @@
+package com.guy.class13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.majorik.sparklinelibrary.SparkLineLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
