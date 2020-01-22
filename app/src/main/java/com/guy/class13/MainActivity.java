090902a12796ce422a8472ef5da6ab438d1dc466
Guy <guyskv@gmail.com>
2020-01-22 19:45:11 +0200

    initial commit.

--- app/src/main/java/com/guy/class13/MainActivity.java
+++ app/src/main/java/com/guy/class13/MainActivity.java
@@ -2,27 +2,45 @@ package com.guy.class13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
+import android.graphics.RectF;
 import android.os.Bundle;
 
-import com.majorik.sparklinelibrary.SparkLineLayout;
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
-import lecho.lib.hellocharts.model.Line;
-import lecho.lib.hellocharts.model.LineChartData;
-import lecho.lib.hellocharts.model.PointValue;
-import lecho.lib.hellocharts.view.LineChartView;
-
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        WeekView mWeekView = (WeekView) findViewById(R.id.weekView);
 
+        mWeekView.setOnEventClickListener(new WeekView.EventClickListener() {
+            @Override
+            public void onEventClick(WeekViewEvent event, RectF eventRect) {
+
+            }
+        });
+
+        mWeekView.setMonthChangeListener(new MonthLoader.MonthChangeListener() {
+            @Override
+            public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+                List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+
+                Calendar calendar = Calendar.getInstance();
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.add(Calendar.HOUR, 1);
+                WeekViewEvent someEvent = new WeekViewEvent(1,"someEvent",calendar,calendar1);
+                events.add(someEvent);
+                return events;
+            }
+        });
     }
 }
